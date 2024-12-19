e7ad365e5e484b382d6ed1ad84bff76ea33c2297
omGosai <108611177+omGosai@users.noreply.github.com>
2024-12-19 14:21:30 +0530

    init

---
+++ com.auctionsystem/com.auctionsystem/src/test/java/com/practice/onlineauctionsystem/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.onlineauctionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
